b68dea737a2ca4a3f0e7ed6493e37a069ca917bf
Zei <Zei@gmail.com>
2024-06-20 21:45:32 -0500

    Cola update

--- U1/Extra/main.cpp
+++ U1/Extra/main.cpp
@@ -10,20 +10,16 @@ int main() {
     size_t N;
     int X;
 
-    // Crear la cola
     cola_enteros.Crear_cola(ok);
-    std::cout << "Cola creada. ¿Está vacía? " << (resp ? "Sí" : "No") << std::endl;
+    std::cout << "" << (resp ? "Sí" : "No") << std::endl;
 
-    // Encolar valores
     cola_enteros.Queue(10, ok);
     cola_enteros.Queue(20, ok);
     cola_enteros.Queue(30, ok);
 
-    // Mostrar el tamaño de la cola
     cola_enteros.Tamanio(N);
-    std::cout << "Tamaño de la cola: " << N << std::endl;
+    std::cout << "Cardinalidad: " << N << std::endl;
 
-    // Mostrar el frente de la cola
     try {
         cola_enteros.Dequeue(X, ok);
         std::cout << "Elemento desencolado: " << X << ". Nuevo frente: " << cola_enteros.frente() << std::endl;
@@ -31,9 +27,8 @@ int main() {
         std::cerr << e.what() << std::endl;
     }
 
-    // Limpiar la cola
     cola_enteros.Borrar_cola(ok);
-    std::cout << "Cola limpiada. ¿Está vacía? " << (cola_enteros.estaVacia() ? "Sí" : "No") << std::endl;
+    std::cout << (cola_enteros.estaVacia() ? "Cola Limpia" : "Llena con almenos un elemento") << std::endl;
 
     return 0;
 }
